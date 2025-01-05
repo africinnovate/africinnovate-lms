a4843c6e851d504b2b5d566eaf66205f097b4cec
cjustinobi <justin@Justins-MacBook-Pro.local>
2025-01-05 10:20:19 +0100

    add eslint

--- .eslintrc.config.js
+++ .eslintrc.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  ignorePatterns: ['dist/', 'node_modules/', '.eslintrc.config.js'],
+  ignores: ['dist/', 'node_modules/', '.eslintrc.config.js'],
   settings: {
     next: {
       rootDir: ['apps/*/'],
